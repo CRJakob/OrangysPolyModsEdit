c9cc2da33a0c066c8a4484786492e5b1ab152523
0rangy <86059513+0rangy@users.noreply.github.com>
2025-04-08 09:25:59 -0400

    yee complete

--- husplits/1.0.0/main.mod.js
+++ husplits/1.0.0/main.mod.js
@@ -4,14 +4,79 @@ import { PolyMod, MixinType } from "https://cdn.jsdelivr.net/gh/0rangy/PolyModLo
 class HUSplits extends PolyMod {
     init = (pml) => {
         this.modPmlInstance = pml;
-        console.log(pml.getFromPolyTrack('pP'))
         pml.registerFuncMixin("pP", MixinType.INSERT, `yP(this, eP, "f").notificationAudioEnabled = !0,`, () => {
-            console.log("AAA");
-            car.addCheckpointCallback((e) => {
-                console.log(e);
+            yP(this, eP, "f").addCheckpointCallback((e) => {
+                if(yP(this, HC, "f") === null) return;
+                const curetime = yP(this, eP, "f").getTime();
+                let ghostitme = null;
+                const i = yP(this, wC, "m", mP).call(this);
+                null != i && i.checkpointTimes.length > e && (ghostitme = i.checkpointTimes[e]);
+                const timeDelta = ghostitme.time - curetime.time;
+                console.log(timeDelta.toFixed(3));
+                let uiDiv = document.getElementById("ui");
+                let hintDiv = document.createElement('div');
+                hintDiv.style = `interpolate-size: allow-keywords;
+    --text-color: #fff;
+    --text-disabled-color: #5d6a7c;
+    --surface-color: #28346a;
+    --surface-secondary-color: #212b58;
+    --surface-tertiary-color: #192042;
+    --surface-transparent-color: rgba(40, 52, 106, 0.5);
+    --button-color: #112052;
+    --button-hover-color: #334b77;
+    --button-active-color: #151f41;
+    --button-disabled-color: #313d53;
+    scrollbar-color: #7272c2 #223;
+    cursor: none;
+    -webkit-tap-highlight-color: transparent;
+    user-select: none;
+    font-style: italic;
+    font-family: ForcedSquare, Arial, sans-serif;
+    line-height: 1;
+    padding: 0 280px;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    box-sizing: border-box;
+    text-align: center;
+    color: #fff;
+    text-shadow: 0 0 5px #000;
+    pointer-events: none;
+    top: 150px;
+    opacity: 1;
+    transition: opacity 0.25s ease-in-out, top 0.25s ease-in-out;`;
+                let splitDiv = document.createElement('div');
+                splitDiv.style = `    interpolate-size: allow-keywords;
+    --text-color: #fff;
+    --text-disabled-color: #5d6a7c;
+    --surface-color: #28346a;
+    --surface-secondary-color: #212b58;
+    --surface-tertiary-color: #192042;
+    --surface-transparent-color: rgba(40, 52, 106, 0.5);
+    --button-color: #112052;
+    --button-hover-color: #334b77;
+    --button-active-color: #151f41;
+    --button-disabled-color: #313d53;
+    scrollbar-color: #7272c2 #223;
+    cursor: none;
+    -webkit-tap-highlight-color: transparent;
+    user-select: none;
+    text-align: center;
+    color: #fff;
+    text-shadow: 0 0 5px #000;
+    pointer-events: none;
+    font-style: italic;
+    font-family: ForcedSquare, Arial, sans-serif;
+    line-height: 1;
+    font-size: 32px;`;
+                splitDiv.innerHTML = `<p style="color: ${timeDelta <= 0 ? '#f55' : '#5f5'}">${timeDelta <= 0 ? "+" : ""}${timeDelta.toFixed(3)*-1}</p>`
+                hintDiv.appendChild(splitDiv);
+                uiDiv.appendChild(hintDiv);
+                setTimeout(() => {
+                    hintDiv.remove();
+                }, 2000)
             });
         })
-        console.log(pml.getFromPolyTrack('pP'))
     }
 }
 
