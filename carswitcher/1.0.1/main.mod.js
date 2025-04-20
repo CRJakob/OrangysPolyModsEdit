c4fb1cffca4f7fda58d295a2b912ffb5a5c532c7
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-20 23:08:04 +0200

    add huracan

--- carswitcher/1.0.1/main.mod.js
+++ carswitcher/1.0.1/main.mod.js
@@ -11,6 +11,10 @@ class OrangysCarSwitcherMod extends PolyMod {
                 "url": "models/car.glb",
             },
             {
+                "name": "Huracan",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/huracan.glb`,
+            },
+            {
                 "name": "Trackmania 2020",
                 "url": `${this.modBaseUrl}/${this.modVersion}/assets/tm2020.glb`,
             },
