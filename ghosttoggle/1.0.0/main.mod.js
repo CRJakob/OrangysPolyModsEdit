ce0eea5241b0a773dff54e95f779a8ec797b24e1
0rangy <86059513+0rangy@users.noreply.github.com>
2025-04-22 20:00:17 -0400

    NEW ghost toggle mod

---
+++ ghosttoggle/1.0.0/main.mod.js
@@ -0,0 +1,14 @@
+import { PolyMod, MixinType } from "https://cdn.jsdelivr.net/gh/0rangy/PolyModLoader@0.5.0/PolyModLoader.js"
+
+class GhostToggleMod extends PolyMod {
+    init = (pml) => {
+        this.pml = pml;
+        this.ghostEnabled = true;
+        pml.registerBindCategory("Ghost Toggle Mod");
+        pml.registerKeybind("Toggle Ghost", "ghostToggle", "keydown", "KeyO", null, (e) => { this.ghostEnabled = !this.ghostEnabled; console.log(this.ghostEnabled) });
+        pml.registerClassMixin("AP.prototype", "update",MixinType.REPLACEBETWEEN, `e.car.getTime().numberOfFrames`, `e.car.getTime().numberOfFrames`, `(ActivePolyModLoader.getMod("${this.id}").ghostEnabled ? e.car.getTime().numberOfFrames : 0)`)
+        pml.registerClassMixin("AP.prototype", "update",MixinType.INSERT, `e.car.setCarState(t)`, `;if(!ActivePolyModLoader.getMod("${this.id}").ghostEnabled) break;`)
+    }
+}
+
+export let polyMod = new GhostToggleMod();
