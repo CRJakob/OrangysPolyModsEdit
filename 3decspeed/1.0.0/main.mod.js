400a542f04dbdf43e9acc928fc91c207871f5f7c
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-05-05 12:03:21 +0200

    Update main.mod.js

--- 3decspeed/1.0.0/main.mod.js
+++ 3decspeed/1.0.0/main.mod.js
@@ -2,7 +2,7 @@ import { PolyMod, MixinType } from "https://pml.orangy.cfd/PolyTrackMods/PolyMod
 
 class ThreeDecimalSpeedometer extends PolyMod {
     init = (pml) => {
-        pml.registerClassMixin("TT.prototype", "update", MixinType.REPLACEBETWEEN, "Math.trunc(n).toString();", "Math.trunc(n).toString();", "n.toFixed(6);")
+        pml.registerClassMixin("TT.prototype", "update", MixinType.REPLACEBETWEEN, "Math.trunc(n).toString();", "Math.trunc(n).toString();", "n.toFixed(12);")
     }
 }
 
